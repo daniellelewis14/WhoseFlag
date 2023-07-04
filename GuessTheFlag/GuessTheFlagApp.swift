e4749383f025db7211fb15214518e42747a79b0c
daniellelewis14 <yurts.third_0v@icloud.com>
2023-07-03 21:59:44 -0400

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Danielle Lewis on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
